b1b40ed1053119121ef42a176e36d119c33ad148
Jordan R <jramey@uiowa.edu>
2022-05-01 13:05:01 -0500

    Creating Routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import './categories.stles.scss'
-import Directory from './components/directory/directory.component';
+import Home from "./routes/home/home.component";
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-  const categories = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "Hoodies",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "Sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "Womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "Mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-  
-  return (
-      <Directory categories={categories} />
+  return(
+    <Routes>
+      <Route path='/' element={<Home />}/>
+    </Routes>
   );
 }
 
