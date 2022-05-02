777e69cd86bfeb3df0fd73b6f80a38487fd0fb9d
Jordan R <jramey@uiowa.edu>
2022-05-02 16:29:47 -0500

    set up logic for users collections in firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyAWjEgmca11b6olATBH9cBRemJ-tPCQVyI",
@@ -19,3 +26,31 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+
+    if(!userSnapshot.exists()){
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try{
+            await setDoc(userDocRef,{
+                displayName,
+                email,
+                createdAt
+            }); 
+        }catch(error){
+            console.log('there was an error creating the user', error.message);
+        }
+    }
+
+    return userDocRef;
+  }
