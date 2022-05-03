41bf7440cf913c2c26df76d67695f53815083933
Jordan R <jramey@uiowa.edu>
2022-05-03 14:33:42 -0500

    Finish authentication page

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss'
+import Button from '../button/button.component';
+
+const defaultformFields = {
+    email: '',
+    password: ''
+}
+
+
+const SignInForm = () => {
+    const [formFields, setFormFields] = useState(defaultformFields);
+    const { email, password } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultformFields);
+    }
+
+    const signInWithGoogle = async() => {
+        const {user} = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user);
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try{
+            const response = await signInAuthUserWithEmailAndPassword(email, password);
+            console.log(response);
+            resetFormFields();
+
+        } catch(error){
+            switch(error.code){
+                case 'auth/wrong-password':
+                    alert('Incorrect information was entered');
+                    break;
+                case 'auth/user-not-found':
+                    alert('Incorrect information was entered');
+                    break;
+                default:
+                    console.log(error);
+            }
+            
+        }
+    }
+
+    const handleChange = (event => {
+        const { name, value } = event.target;
+        setFormFields({...formFields,[name]: value});
+    })
+
+    return(
+        <div className='sign-up-container'>
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput label='Email' type = 'email' required onChange={handleChange} name='email' value={email}/>
+
+                <FormInput label='Password' type= 'password' required onChange={handleChange} name='password' value={password}/>
+                <div className='buttons-container'>
+                    <Button type='submit'>Sign In</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default SignInForm;
