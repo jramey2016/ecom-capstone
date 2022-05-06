efa657a27ca2cf0e782e5e4c61202c1091aaf5c0
Jordan R <jramey@uiowa.edu>
2022-05-05 21:05:11 -0500

    finished up adding collection to firebase, added categories page

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,11 @@ import {
     doc,
     getDoc,
     setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
+    DocumentSnapshot
 } from 'firebase/firestore'
 
 
@@ -30,6 +35,33 @@ const firebaseConfig = {
 
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done')
+  }
+
+  export const getCategoriesandDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+      const { title, items } = docSnapshot.data();
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+
+    return categoryMap;
+  }
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {displayName: 'mike'}) => {
     if(!userAuth) return;
